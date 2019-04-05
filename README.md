f4a609bf6ac3304a39d06176860bab1616095a79
mike lorzing <michaellorzing@github.com>
2019-04-05 17:49:16 -0400

    submit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # jQuery-Quiz
+
+History Quiz
+
+Had a bit more trouble with this one than I initially anticipated. With that said, I plan on putting some more work into this over the next few days to get it working the way I want to.
+
+I used basic HTML element, CSS, bootstrap, JS, and jQuery to construct it.
+
+I wanted to balance something visually appealing with a well functioning, short quiz. I think I may have dug myself into a bit of a hole, though. I'm having a particularly difficult time taking the values I've pulled from my answer and comparing them to my correct answers. I've pseudocoded essentially everything that I had planned. I'm just not entirely sure how I'd achieve it. I think I'm fairly close, but missing a few essential components to get it to run the way I'd like.
--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@ var wrongAns=0;
 var unanswered=0;
 var time=30;
 var intervalId;
-var answerArray=[]
+var answer=[]
 
 
 //create an object with the questions in the quiz
@@ -14,32 +14,32 @@ var quizQuestions =[
   {
     question: "Who is regarded as the first European to land in North America?",
     choices: ["Christopher Columbus", "Magellan", "Leif Erikson"],
-    correctAnswer: "Leif Erikson"
+    correctAnswer: "Leif Erikson",
   },
   {
     question: "Which is the longest war in US history?",
     choices: ["Afghan War", "Cold War", "Vietnam War"],
-    correctAnswer: "Afghan War"
+    correctAnswer: "Afghan War",
   },
   {
     question: "The first car accident in US history occured in which state?",
     choices: ["New York", "Ohio", "Rhode Island"],
-    correctAnswer: "Ohio"
+    correctAnswer: "Ohio",
   },
   {
     question: "How many people were the Aztecs estimated to have sacrificed every year?",
     choices: ["3,000", "80,000", "250,000"],
-    correctAnswer: "250,000"
+    correctAnswer: "250,000",
   },
   {
     question: "The US Constitution was written in which year?",
     choices: ["1776", "1787", "1792"],
-    correctAnswer: "1787"
+    correctAnswer: "1787",
   },
   {
     question: "Which artist painted the Sistene chapel?",
     choices: ["Da Vinci", "Rembrandt", "Michelangelo"],
-    correctAnswer: "Michelangelo"
+    correctAnswer: "Michelangelo",
   },
 
 ];
@@ -80,15 +80,16 @@ function quizRun(){
       var jerrysKids= `JerrysKids${i}${j}`
       var morty= `#JerrysKids${i}${j}`
       $("#mainQuiz").append(`<p><input class="form-check-input" value="${choice}" type="radio" id="${jerrysKids}" name="JerrysKids${i}">${choice}</p>`);
-      $(morty).on("click", function(e){
+      $(morty).select(function(e){
         e.preventDefault();
-        console.log(e)
       })
     } 
-    
-    
-
   }
+  $("#mainQuiz").on("change", ".form-check-input", function(){
+    var questionIndex = $(this).attr("name")
+    var answer=$(this).val();
+    
+  })
 }
 
 
@@ -102,26 +103,26 @@ function newQuiz(){
   runClock();
   $("#mainQuiz").append("<button type='button' class='btn btn-primary btn-lg' id='finishButton'>Finished!</button>");
   finishButton();
-  //scoreCounter();
+  scoreCounter();
 
 }
 
 //display right, wrong, and unanswered
 
-//function scoreCounter(){
-  //if (choices===correctAnswer){
-   // rightAns++;
- // } else if (choices!==correctAnswer){
-    //wrongAns++;
- // } else (correctAnswer===undefined)
-   // unanswered++;
-  
-  //create finishButton to trigger new html with correct, wrong, unanswered totals. Button to bring you back to new Quiz
-//}
+function scoreCounter(){
+  for (var a = 0; i < quizQuestions[i].correctAnswer.length; a++ ){
+    var correctAnswer=quizQuestions[i].correctAnswer[a];
+  if (answer===quizQuestions.correctAnswer[a]){
+    rightAns++;
+  } else if (answer!==quizQuestions.correctAnswer[a]){
+    wrongAns++;
+  } else (unanswered++);
+  console.log(rightAns)
+ }}
 
 function finishButton(){
   $("#finishButton").on("click", finalScore)
-    //scoreCounter();  
+    scoreCounter();  
   }
   
 
@@ -131,6 +132,7 @@ function finalScore(){
   $("#mainQuiz").html(
     "<p> Correct answers= 4 <br> Wrong answers= 4 <br> Unanswered= 4 </p> <br> <button type='button' class='btn btn-primary btn-lg' id='startButton'>Play Again!</button>");
     startGame();
+  
 }
 //button click to play again
 
